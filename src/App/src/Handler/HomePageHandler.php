7e3af0d6b2f1020d3dee8c196473de8fd6226762
Matthew Setter <matthew@matthewsetter.com>
2021-06-28 11:03:21 +0200

    Combine the code into the main handler
    The intention here is to simplify/condense the code structure so that it
    works better from the perspective of a tutorial.

--- src/App/src/Handler/HomePageHandler.php
+++ src/App/src/Handler/HomePageHandler.php
@@ -11,43 +11,89 @@ use Laminas\Diactoros\Response\XmlResponse;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Twilio\TwiML\MessagingResponse;
 
-/**
- * Class HomePageHandler
- * @package App\Handler
- */
+use function json_decode;
+use function stream_get_contents;
+
 class HomePageHandler implements RequestHandlerInterface
 {
-    private CityWeatherRequestParser $requestParser;
-    private WeatherService $weatherService;
-    private WeatherResponse $message;
-
-    public function __construct(
-        WeatherService $weatherService,
-        CityWeatherRequestParser $weatherParser,
-        WeatherResponse $message
-    ) {
-        $this->message = $message;
-        $this->requestParser = $weatherParser;
-        $this->weatherService = $weatherService;
-    }
+    public const BASE_URI = 'https://api.weatherapi.com/v1/current.json';
+    public const BODY_PATTERN = "/What is the weather like in (?P<city>[a-z\-' ].*) today\?/i";
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $body = $request->getParsedBody()['Body'];
-        $city = $this->requestParser->getCityFromRequest($body);
+        $city = $this->getCityFromRequest($body);
 
         if (!$city) {
             return new XmlResponse(
-                (string)$this->message->getErrorResponse('Sorry, but I could not determine the city you asked for.')
+                (string)$this->getErrorResponse(
+                    'Sorry, but I could not determine the city you asked for.'
+                )
             );
         }
 
-        $weatherData = $this->weatherService->getWeatherData($city);
+        $weatherData = $this->getWeatherData($city);
         if (property_exists($weatherData, 'error')) {
-            return new XmlResponse((string)$this->message->getErrorResponse($weatherData->error->message));
+            return new XmlResponse((string)$this->getErrorResponse($weatherData->error->message));
         }
 
-        return new XmlResponse((string)$this->message->getSuccessResponse($weatherData));
+        return new XmlResponse((string)$this->getSuccessResponse($weatherData));
+    }
+
+    private function getCityFromRequest(?string $body): ?string
+    {
+        return (preg_match(self::BODY_PATTERN, $body, $matches))
+            ? $matches['city']
+            : null;
+    }
+
+    private function getWeatherData(string $city): object
+    {
+        $apiKey = $_ENV['WEATHERAPI_API_KEY'];
+        $queryString = http_build_query([
+            'key' => $apiKey,
+            'q' => $city,
+            'aqi' => 'no'
+        ]);
+        $requestUri = sprintf('%s?%s', self::BASE_URI, $queryString);
+
+        $fh = fopen($requestUri, 'rb');
+        $weatherData = json_decode(stream_get_contents($fh));
+        fclose($fh);
+
+        return $weatherData;
+    }
+
+    private function getSuccessResponse(object $weatherData): MessagingResponse
+    {
+        $responseString = <<<EOF
+In %s (%s, %s), today, it's %d degrees celsius, but feels like %d, with a humidity of %d percent.
+The wind is currently %d kp/h from the %s.
+EOF;
+        $response = new MessagingResponse();
+        $response->message(
+            sprintf(
+                $responseString,
+                $weatherData->location->name,
+                $weatherData->location->region,
+                $weatherData->location->country,
+                $weatherData->current->temp_c,
+                $weatherData->current->feelslike_c,
+                $weatherData->current->humidity,
+                $weatherData->current->wind_kph,
+                $weatherData->current->wind_dir
+            )
+        );
+        return $response;
+    }
+
+    private function getErrorResponse(string $message): MessagingResponse
+    {
+        $response = new MessagingResponse();
+        $response->message($message);
+
+        return $response;
     }
 }
