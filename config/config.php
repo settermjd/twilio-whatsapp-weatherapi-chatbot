cc713a641d8703a82e70ab9de15743de533801aa
Matthew Setter <matthew@matthewsetter.com>
2021-12-15 21:09:15 +0100

    Add Mezzio Tooling's config provider

--- config/config.php
+++ config/config.php
@@ -14,6 +14,7 @@ $cacheConfig = [
 ];
 
 $aggregator = new ConfigAggregator([
+    \Mezzio\Tooling\ConfigProvider::class,
     \Mezzio\Helper\ConfigProvider::class,
     \Mezzio\Router\FastRouteRouter\ConfigProvider::class,
     \Laminas\HttpHandlerRunner\ConfigProvider::class,
